fb073836a90b985052100498533066824217b81f
bayron <bayron_fabian@hotmail.com>
2018-10-30 06:17:19 -0600

    Se modifica CSS y se agrega login

--- Perris/views.py
+++ Perris/views.py
@@ -4,6 +4,11 @@ from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from .forms import LoginForm
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -40,5 +45,23 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'Perris/post_edit.html', {'form': form})
 
-
-
+def login_page(request):
+    message = None
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    message = "Te has identificado correctamente"
+                else:
+                    message = "Tu usuario esta inactivo"
+            else:
+                message = "Nombre de usuario y/o password incorrecto"
+    else:
+        form = LoginForm()
+    return render_to_response('login.html', {'message': message, 'form': form},
+                                context_instance=RequestContext(request))
