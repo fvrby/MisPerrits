dfd9a3e70fef139cd8bb8b64a2d781c1a4b9790b
bayron <bayron_fabian@hotmail.com>
2018-10-31 18:34:49 -0600

    se agregan nuevos modelos adoptar y adoptado

--- Perris/models.py
+++ Perris/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 
+CONDICION = (
+        ('Rescatado', 'Rescatado'),
+        ('Disponible', 'Disponible'),
+        ('Adoptado', 'Adoptado'),
+    )
+
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -18,3 +24,19 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Adoptar(models.Model):
+	correo = models.EmailField()
+	run = models.CharField(max_length=12, primary_key=True)
+	nombreCompleto = models.CharField(max_length=150)
+	telefono = models.CharField(max_length=12)
+	def __str__(self):
+		return self.correo
+
+class Adoptado(models.Model):
+	foto = models.ImageField()
+	nombre = models.CharField(max_length=100)
+	raza = models.CharField(max_length=100)
+	descripcion = models.TextField()
+	estado = models.CharField(max_length=10, choices=CONDICION)
+	def __str__(self):
+		return self.nombre
