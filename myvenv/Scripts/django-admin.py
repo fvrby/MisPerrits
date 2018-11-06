c7b9cc2592db78227e5d1f8d4a3bded514bcd66b
bayron <bayron_fabian@hotmail.com>
2018-11-06 07:27:26 -0600

    profe no lea esto, deje la caga

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\benji\onedrive\escritorio\misperris\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
